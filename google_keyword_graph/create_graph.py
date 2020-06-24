330d6353b4ff9db8e159401cd197704086039524
tung491 <dosontung007@gmail.com>
2020-06-24 11:37:48 +0700

    The initial commit

---
+++ google_keyword_graph/create_graph.py
@@ -0,0 +1,54 @@
+import sqlite3
+import networkx as nx
+
+
+def gen_nodes_and_edges():
+    conn = sqlite3.connect('keywords.db')
+    cursor = conn.cursor()
+    nodes = cursor.execute('''select target, count(*) from keywords group by target''').fetchall()
+    nodes = [(node[0], {'count': node[1]}) for node in nodes]
+
+    suggestions = cursor.execute('SELECT * from keywords').fetchall()
+    edges = []
+    weights = {suggestion[:2]: suggestion[2] for suggestion in suggestions}
+    for suggestion in suggestions:
+        source, target, weight = suggestion
+        total_weight = weight + weights.get((target, source), 0)
+        distance = 11 - total_weight
+        edge = (source, target, {'weight': total_weight, 'distance': distance})
+        edges.append(edge)
+    return nodes, edges
+
+
+def gen_graph(keyword):
+    nodes, edges = gen_nodes_and_edges()
+    G = nx.Graph()
+    G.add_nodes_from(nodes)
+    G.add_edges_from(edges)
+
+    # BUILD THE EGO GRAPH FOR TENSORFLOW
+    EG = nx.ego_graph(G, keyword, distance='distance', radius=23)
+
+    # FIND THE 2-CONNECTED SUBGRAPHS
+    subgraphs = nx.algorithms.connectivity.edge_kcomponents.k_edge_subgraphs(EG, k=3)
+
+    # GET THE SUBGRAPH THAT CONTAINS TENSORFLOW
+    for s in subgraphs:
+        if keyword in s:
+            break
+    pruned_EG = EG.subgraph(s)
+    return pruned_EG
+
+
+def write_to_csv():
+    nodes, edges = gen_nodes_and_edges()
+    with open('points.csv', 'w') as f:
+        for node in nodes:
+            f.write(f"{node[0]}, {node[1]['count']}\n")
+    with open('links.csv', 'w') as f:
+        for edge in edges:
+            f.write(f"{edge[0]}, {edge[1]}, {edge[2]['weight']}\n")
+
+
+if __name__ == '__main__':
+    write_to_csv()
---
+++ google_keyword_graph/get_suggestions.py
@@ -0,0 +1,98 @@
+import sqlite3
+
+import requests
+from xml.etree.ElementTree import fromstring
+from collections import Counter
+from typing import Tuple
+
+URL = 'https://suggestqueries.google.com/complete/search'
+
+conn = sqlite3.connect('keywords.db')
+cursor = conn.cursor()
+
+
+def create_table() -> None:
+    cursor.execute('''DROP TABLE IF EXISTS keywords''')
+    cursor.execute('''CREATE TABLE keywords
+    (source VARCHAR,
+    target VARCHAR,
+    weight INTEGER
+    )
+    ''')
+
+
+def insert(suggestion: Tuple[str, str, int]) -> None:
+    cursor.execute('''INSERT INTO keywords
+    (source, target, weight) VALUES
+    (?, ?, ?)''', suggestion)
+
+
+def get_suggestions(keyword: str) -> list:
+    if not keyword:
+        return []
+    params = {
+        'output': 'toolbar',
+        'gl': 'us',
+        'hl': 'en',
+        'q': f'{keyword} vs '
+    }
+
+    r = requests.get(URL, params=params)
+    suggestions = []
+    if r.ok:
+        tree = fromstring(str(r.content, 'latin-1'))
+        weight = 5
+        accepted_terms = []
+        for suggest in tree.findall('CompleteSuggestion'):
+            text = suggest.find('suggestion').get('data')
+            counter = Counter(text)
+            if text == keyword or counter['vs'] > 1:
+                continue
+            words = text.split()
+            if 'vs' in words:
+                words.remove('vs')
+            if words[0] == keyword:
+                text = ' '.join(words[1:])
+            else:
+                text = ' '.join(words)
+            exist_previous_term = any(term in text for term in accepted_terms)
+            if not text or keyword in text or exist_previous_term:
+                continue
+
+            items = (keyword, text, weight)
+            suggestions.append(items)
+            accepted_terms.append(text)
+            weight -= 1
+            if weight == 0:
+                break
+    return suggestions
+
+
+def get_sub_suggestions(depth: int, suggestions: list):
+    if depth == 0:
+        return
+    for suggestion in suggestions:
+        sub_suggestions = get_suggestions(suggestion[1])
+        previous_terms = cursor.execute('''SELECT DISTINCT source
+        from keywords
+        ''').fetchall()
+        previous_keywords = [previous_term[0] for previous_term in previous_terms]
+        accepted_terms = []
+        for sub_suggestion in sub_suggestions:
+            if sub_suggestion[1] not in previous_keywords:
+                accepted_terms.append(sub_suggestion)
+            insert(sub_suggestion)
+        get_sub_suggestions(depth - 1, accepted_terms)
+
+
+def store_suggestions(keyword, depth):
+    create_table()
+    suggestions = get_suggestions(keyword)
+    for suggestion in suggestions:
+        insert(suggestion)
+    get_sub_suggestions(depth, suggestions)
+    conn.commit()
+
+
+if __name__ == '__main__':
+    store_suggestions('google', 4)
